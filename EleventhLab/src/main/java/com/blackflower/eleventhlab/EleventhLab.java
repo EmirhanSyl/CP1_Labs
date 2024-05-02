06817d67f3d615c2f4ee66d6d67e56ca1f65e3ab
EmirhanSyl <61618968+EmirhanSyl@users.noreply.github.com>
2024-05-02 21:20:59 +0300

    Move All Seperate Repositories into One Repo

---
+++ EleventhLab/src/main/java/com/blackflower/eleventhlab/EleventhLab.java
@@ -0,0 +1,18 @@
+package com.blackflower.eleventhlab;
+
+public class EleventhLab {
+
+    public static void main(String[] args) {
+        Material material0 = new Material();
+        material0.price = 2;
+        material0.title = "Mat1";
+        
+        Material material1 = new Material();
+        material1.price = 2;
+        material1.title = "Mat1";
+        
+        Material material2 = new Material();
+        material2.price = 2;
+        material2.title = "Mat1";
+    }
+}
